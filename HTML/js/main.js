a750ba97f3abe06b72eed5f77d0dbe8652e98c86
JohanAcero <johanacero8@gmail.com>
2024-10-04 21:49:50 -0500

    Añadi demasiadas cosas, agregue un nuevo menu_item para poder visualizar los guardianes activos, de igual forma añadi el javascript para la funcionalidad del popup de mascotas, ya que asi se podra verificar cuando este activo y cuando no para asi mostrar y tener dicha funcionalidad.
    Agregamos la parte responsive de la sidebar para ciertos
    dispositivos en la parte del css.

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,3 +1,5 @@
+
+//================ SCROLL DE MASCOTAS RECOMENDADAS ===========================
 const scroller = document.querySelectorAll(".scroll");
 
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) { /* Le decimos que se use solo cuando el usuario no tenga desactivada las animaciones */
@@ -25,3 +27,54 @@ function addAnimation() {
         });
     });
 }
+
+
+//================= SIDEBAR =================================
+
+const MenuItem = document.querySelectorAll('.menu-item');
+
+
+//---------------FUNCION PARA VERIFICAR EL ESTADO ACTIVO O NO DEL POPUP
+const mascotasPopup = document.querySelector('.mascotas-popup');
+
+//------------ FUNCION DE GUARDIANES
+const guardianesBox = document.querySelector('#guardianes-box'); //FUNCION PARA EL COLOR DE LA CAJA
+const guardianesBoxColor = document.querySelector('.guardianes');  //PARA SELECCIONAR LA CLASE DEL COLOR DE LA CAJA
+const guardianBuscar = document.querySelectorAll('.guardianes');
+
+//--------- REMOVER LA CLASE ACTIVO DE TODOS LOS ITEMS DEL MENU
+const cambiarActivo = () => {
+    MenuItem.forEach(item => {
+        item.classList.remove('activo');
+    })
+}
+
+MenuItem.forEach(item =>{
+    item.addEventListener('click', () => {
+        cambiarActivo(); //Añadimos la funcion de camibar el activo para que no queden todos activos
+        item.classList.add('activo');
+
+        // AÑADIMOS LA FUNCIONALIDAD DE DESPLEGAR EL POPUP DE MASCOTAS CUANDO SE LE DE CLIK
+
+        if(item.id === 'popup-mascotas'){ // FUNCION DEL DISPLAY BLOCK O NONE DEPENDIENDO EL ESTADO DEL POPUP
+            if (mascotasPopup.style.display === 'block') {
+                mascotasPopup.style.display = 'none';
+            } else {
+                mascotasPopup.style.display = 'block';
+            } 
+        } else {
+            mascotasPopup.style.display = 'none';
+        }
+    })
+})
+
+
+// ======= CAJA DE GUARDIANES =================
+
+guardianesBox.addEventListener('click', () => {
+    guardianesBoxColor.style.boxShadow = '0 0 1rem var(--color-primary)'; //PARA QUE CAMBIE DE COLOR CON EL CLICK AL CLOR PRIMARIO
+
+    setTimeout(() => {
+        guardianesBoxColor.style.boxShadow = 'none';
+    }, 2000);
+})
